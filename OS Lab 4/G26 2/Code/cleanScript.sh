065503db34e148cd05951b743f8eebfbd9ef957a
Yogesh Kumar <yogeshkumar98103@hotmail.com>
2020-11-25 12:49:34 +0530

    updated readme.txt

--- OS Lab 4/G26 2/Code/cleanScript.sh
+++
@@ -1,8 +0,0 @@
-declare -a arr=("zfs_compress" "zfs_nocompress" "zfs_encrypt" "zfs_noencrypt")
-
-for i in "${arr[@]}"
-do
-   sudo zfs unmount "$i"
-   sudo zpool destroy "$i"
-done
-rm ~/zfs_*.img
